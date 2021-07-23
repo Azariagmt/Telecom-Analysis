f2a66287ef4c4ed531d06c4719c1a4be3cb1e343
Azariagmt <azariagebremichael10@gmail.com>
2021-07-23 09:05:21 +0300

    adds experience page

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
 
 @app.route('/dashboard')
 def dashboard():
